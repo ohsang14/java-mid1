ef7578cb82dd1cc2fd8f964e5c9cb1da8cb69fcf
ohsang14 <ast6108@naver.com>
2024-04-06 18:01:28 +0900

    불변 객체 - 값 변경

---
+++ src/lang/immutable/test/ImmuableMyDate.java
@@ -0,0 +1,14 @@
+package lang.immutable.test;
+
+public class ImmuableMyDate {
+    private final int year;
+    private final int month;
+    private final int day;
+
+    public ImmuableMyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+}
