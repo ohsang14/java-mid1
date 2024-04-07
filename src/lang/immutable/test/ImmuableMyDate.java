981961b4db32a7981a2f3eb175aa777a135673f6
ohsang14 <ast6108@naver.com>
2024-04-07 16:39:43 +0900

    불변.

--- src/lang/immutable/test/ImmuableMyDate.java
+++ src/lang/immutable/test/ImmuableMyDate.java
@@ -10,5 +10,22 @@ public class ImmuableMyDate {
         this.month = month;
         this.day = day;
     }
+    public ImmuableMyDate withYear(int newYear){
+        return new ImmuableMyDate(newYear,month,day);
+    }
+
+
+    public ImmuableMyDate withMonth(int newmonth) {
+        return new ImmuableMyDate(year,newmonth,day);
+    }
+
+    public ImmuableMyDate withDay(int newday) {
+        return new ImmuableMyDate(year,month,newday);
+    }
+
+    @Override
+    public String toString() {
+        return year + "-" + month + "-" + day;
+    }
 
 }
---
+++ src/lang/immutable/test/ImmutableMyDateMain.java
@@ -0,0 +1,17 @@
+package lang.immutable.test;
+
+public class ImmutableMyDateMain {
+    public static void main(String[] args) {
+        ImmuableMyDate date1 = new ImmuableMyDate(2024, 1, 1);
+        ImmuableMyDate date2 = date1;
+        System.out.println("date1 = " + date1);
+        System.out.println("date2 = " + date2);
+
+        System.out.println("2025 -> date1");
+        //date1.setYear(2025);
+//        date2 = new ImmuableMyDate(2025,1,1);
+        date1 = date1.withYear(2025);
+        System.out.println("date1 = " + date1);
+        System.out.println("date2 = " + date2);
+    }
+}
