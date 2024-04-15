aa5cb62337d0c56793a00707eec5538b6a685ef0
ohsang14 <ast6108@naver.com>
2024-04-15 19:11:16 +0900

    .

---
+++ src/lang/clazz/ClassMetaMain.java
@@ -0,0 +1,11 @@
+package lang.clazz;
+
+public class ClassMetaMain {
+    public static void main(String[] args) {
+        // Class 조회
+        Class clazz = String.class; // 1. 클래스에서 조회
+        Class clazz1 =new String().getClass(); // 2.인스턴스에서 조회
+        =Class.forName("java.lang.String");
+
+    }
+}
