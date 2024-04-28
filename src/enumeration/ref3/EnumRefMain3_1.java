251cb283839ec74c036117d96cc5dd8b35610479
ohsang14 <ast6108@naver.com>
2024-04-28 15:57:43 +0900

    열거형 - 리팩토링3

--- src/enumeration/ref3/EnumRefMain3_1.java
+++ src/enumeration/ref3/EnumRefMain3_1.java
@@ -1,10 +1,10 @@
-package enumeration.ref2;
+package enumeration.ref3;
 
 
-public class EnumRefMain1 {
+public class EnumRefMain3_1 {
     public static void main(String[] args) {
         int price = 10000;
-     DiscountService discountService = new DiscountService();
+        DiscountService discountService = new DiscountService();
         int basic = discountService.discount(Grade.BASIC, price);
         int gold = discountService.discount(Grade.GOLD, price);
         int diamond = discountService.discount(Grade.DIAMOND, price);
