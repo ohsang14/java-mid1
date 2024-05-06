989b2c64cc2ce3b21d2446740da94a00b60cba93
ohsang14 <ast6108@naver.com>
2024-05-06 18:35:05 +0900

    날짜 date

---
+++ src/time/LocalDateMain.java
@@ -0,0 +1,16 @@
+package time;
+
+import java.time.LocalDate;
+
+public class LocalDateMain {
+    public static void main(String[] args) {
+        LocalDate nowDate = LocalDate.now();
+        LocalDate ofDate = LocalDate.of(2013,11,11);
+        System.out.println("오늘 날짜: " + nowDate);
+        System.out.println("지정 날짜: " + ofDate);
+
+        // 계산 (불변)
+        ofDate = ofDate.plusDays(10);
+        System.out.println("저정 날짜 + 10d : " + ofDate );
+    }
+}
