2569d7fc197e91b988d1be5e16dd9f39adeb5541
ohsang14 <ast6108@naver.com>
2024-05-30 02:30:17 +0900

    getclass 수정
    수정

--- src/nested/inner/InnerOuterMain.java
+++ src/nested/inner/InnerOuterMain.java
@@ -4,9 +4,10 @@ public class InnerOuterMain {
     public static void main(String[] args) {
 
         InnerOuter outer = new InnerOuter();
+
         InnerOuter.Inner inner = outer.new Inner();
         inner.print();
 
-        System.out.println("inner.getClass() = " + inner.getClass());
+        System.out.println("innerClass = " + inner.getClass());
     }
 }
