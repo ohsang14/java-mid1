d0b2d3a604fb23274beb3487f7e4b188561edad2
ohsang14 <ast6108@naver.com>
2024-06-09 17:22:27 +0900

    공유 참조와 사이드 이펙트

--- src/lang/immutable/address/RefMain_3.java
+++ src/lang/immutable/address/RefMain_3.java
@@ -13,8 +13,8 @@ public class RefMain_3 {
         System.out.println("a = " + a);
         System.out.println("b = " + b);
     }
-    private static void change(Address address , String changeAddress){
-        System.out.println("주소 값을 변경합니다 "  +  changeAddress);
-        address.setValue(changeAddress);
+   private static void change(Address address , String changeAddress){
+       System.out.println("주소 값을 변경합니다 --> " + changeAddress);
+       address.setValue(changeAddress);
     }
 }
