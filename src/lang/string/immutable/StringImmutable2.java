54655737c57fb704afa1920f3956e0cfc29cec98
ohsang14 <ast6108@naver.com>
2024-06-15 17:17:44 +0900

    String 클레스( 불변객체)

--- src/lang/string/immutable/StringImmutable2.java
+++ src/lang/string/immutable/StringImmutable2.java
@@ -3,7 +3,7 @@ package lang.string.immutable;
 public class StringImmutable2 {
     public static void main(String[] args) {
         String str1 = "hello";
-        String str2 = str1.concat("java");
+        String str2 = str1.concat(" java");
         System.out.println("str = " + str2);
     }
 }
