e501f73b4e161932422c10b7362c4ae3c8907edf
ohsang14 <ast6108@naver.com>
2024-06-15 15:18:40 +0900

    불변 객체 - 값 변경

--- src/lang/immutable/change/ImmutableObj.java
+++ src/lang/immutable/change/ImmutableObj.java
@@ -1,16 +1,16 @@
 package lang.immutable.change;
 
 public class ImmutableObj {
+
     private final int value;
 
     public ImmutableObj(int value) {
         this.value = value;
     }
-
-    public ImmutableObj add(int addValue) {
-        int result = value + addValue;
-        return new ImmutableObj(result);
-    }
+public ImmutableObj add(int addValue){
+        int result  = value + addValue;
+        return new ImmutableObj(result); // 새로운 객체 생성
+}
 
     public int getValue() {
         return value;
