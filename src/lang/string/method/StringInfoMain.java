d88fb9f3dc65f272656664b0b9da090dcd0e755c
ohsang14 <ast6108@naver.com>
2024-06-16 15:41:19 +0900

    String 클레스 주요메서드 1

--- src/lang/string/method/StringInfoMain.java
+++ src/lang/string/method/StringInfoMain.java
@@ -5,10 +5,10 @@ public class StringInfoMain {
         String str = "Hello, Java!";
         System.out.println("문자열의 길이: " + str.length());
         System.out.println("문자열이 비어 있는지: " + str.isEmpty());
-        System.out.println("문자열이 공백인지: " + str.isBlank());
-        System.out.println("문자열이 비어 있거나 공백인지: " + "      ".isBlank());
+        System.out.println("문자열이 비어 있거나, 공백인지1: " + str.isBlank());
+        System.out.println("문자열이 비어 있거나, 공백인지2: " + "         ".isBlank());
 
         char c = str.charAt(7);
-        System.out.println("7번 인덱스의 문자 = " + c);
+        System.out.println("7번째 인덱스의 문자 = " + c);
     }
 }
