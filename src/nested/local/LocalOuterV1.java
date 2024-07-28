ce0554930fdb75d8bf207f6785d1a9a8abddb132
ohsang14 <ast6108@naver.com>
2024-07-28 18:09:02 +0900

    지역 클레스 시작

--- src/nested/local/LocalOuterV1.java
+++ src/nested/local/LocalOuterV1.java
@@ -3,23 +3,20 @@ package nested.local;
 public class LocalOuterV1 {
     private int outInstanceVar = 3;
 
-    public void process(int paraVar) {
+    public void process(int paramVar) {
         int localVar = 1;
-
-
         class LocalPrinter {
             int value = 0;
 
             public void printData() {
-                System.out.println("value= " + value);
-                System.out.println("localVar= " + localVar);
-                System.out.println("pramVar = " + paraVar);
-                System.out.println("outInstanceVar = " + outInstanceVar);
+                System.out.println("value=" + value);
+                System.out.println("localVar=" + localVar);
+                System.out.println("paramVar=" + paramVar);
+                System.out.println("outInstanceVar=" + outInstanceVar);
             }
         }
         LocalPrinter printer = new LocalPrinter();
         printer.printData();
-
     }
 
     public static void main(String[] args) {
--- src/nested/local/LocalOuterV2.java
+++ src/nested/local/LocalOuterV2.java
@@ -3,25 +3,21 @@ package nested.local;
 public class LocalOuterV2 {
     private int outInstanceVar = 3;
 
-    public void process(int paraVar) {
+    public void process(int paramVar) {
         int localVar = 1;
-
-
-        class LocalPrinter implements Printer{
+        class LocalPrinter implements Printer {
             int value = 0;
-
 
             @Override
             public void print() {
-                System.out.println("value= " + value);
-                System.out.println("localVar= " + localVar);
-                System.out.println("pramVar = " + paraVar);
-                System.out.println("outInstanceVar = " + outInstanceVar);
+                System.out.println("value=" + value);
+                System.out.println("localVar=" + localVar);
+                System.out.println("paramVar=" + paramVar);
+                System.out.println("outInstanceVar=" + outInstanceVar);
             }
         }
         LocalPrinter printer = new LocalPrinter();
         printer.print();
-
     }
 
     public static void main(String[] args) {
