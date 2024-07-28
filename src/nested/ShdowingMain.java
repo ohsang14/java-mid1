acfcb52a9b9b1133cd8ef720e7f629e46057ee8e
ohsang14 <ast6108@naver.com>
2024-07-28 17:55:11 +0900

    같은 이름의 바깥 변수 접근

--- src/nested/ShdowingMain.java
+++ src/nested/ShdowingMain.java
@@ -4,12 +4,11 @@ public class ShdowingMain {
     public int value = 1;
     class Inner{
         public int value = 2;
-
-        void go(){
+        void go (){
             int value = 3;
             System.out.println("value = " + value);
             System.out.println("this.value = " + this.value);
-            System.out.println("ShdowingMain.value = " + ShdowingMain.this.value);
+            System.out.println("shdowingMain.value =" + ShdowingMain.this.value);
         }
     }
 
